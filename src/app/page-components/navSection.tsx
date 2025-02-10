4ee0acec62618be5fd38d040a57cadbd44985a97
FDSAP-AZBARAO <FDSAP-RRPOLINTANG@19-SDEV-FDSAP-ABAR.local>
2025-02-10 14:46:45 +0800

    created list of users

--- src/app/page-components/navSection.tsx
+++ src/app/page-components/navSection.tsx
@@ -15,39 +15,39 @@ import {
 
 const components: { title: string; href: string; description: string }[] = [
   {
-    title: "Alert Dialog",
-    href: "/docs/primitives/alert-dialog",
+    title: "Albert Lopez",
+    href: "#",
     description:
-      "A modal dialog that interrupts the user with important content and expects a response.",
+      "Hope you're doing well!",
   },
   {
-    title: "Hover Card",
-    href: "/docs/primitives/hover-card",
+    title: "Sammuel Jackson",
+    href: "#",
     description:
-      "For sighted users to preview content available behind a link.",
+      "Hope you’re having a great day!",
   },
   {
-    title: "Progress",
-    href: "/docs/primitives/progress",
+    title: "Drake",
+    href: "#",
     description:
-      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+      "How’s life treating you?",
   },
   {
-    title: "Scroll-area",
-    href: "/docs/primitives/scroll-area",
-    description: "Visually or semantically separates content.",
+    title: "Kendrick Lamar",
+    href: "#",
+    description: "Hope everything’s good on your end.",
   },
   {
-    title: "Tabs",
-    href: "/docs/primitives/tabs",
+    title: "Timothy Reeves",
+    href: "#",
     description:
-      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+      "Always here if you need to talk.",
   },
   {
-    title: "Tooltip",
-    href: "/docs/primitives/tooltip",
+    title: "Jonathan",
+    href: "#",
     description:
-      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+      "One step at a time, you’ll get there.",
   },
 ]
 
@@ -56,35 +56,19 @@ export function NavMenu() {
     <div className="flex justify-center bg-gray-200 mt-4 p-4">
     <NavigationMenu>
       <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger className="text-xs border-black bg-gray-100 border-gray-100 border rounded-3xl">Message Requests</NavigationMenuTrigger>
+      <NavigationMenuItem>
+          <NavigationMenuTrigger className="text-xs border-black bg-gray-100 border-gray-100 border rounded-3xl">Message Request</NavigationMenuTrigger>
           <NavigationMenuContent>
-            <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-              <li className="row-span-3">
-                <NavigationMenuLink asChild>
-                  <a
-                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
-                    href="/"
-                  >
-                    <div className="mb-2 mt-4 text-lg font-medium">
-                      shadcn/ui
-                    </div>
-                    <p className="text-sm leading-tight text-muted-foreground">
-                      Beautifully designed components built with Radix UI and
-                      Tailwind CSS.
-                    </p>
-                  </a>
-                </NavigationMenuLink>
-              </li>
-              <ListItem href="/docs" title="Introduction">
-                Re-usable components built using Radix UI and Tailwind CSS.
-              </ListItem>
-              <ListItem href="/docs/installation" title="Installation">
-                How to install dependencies and structure your app.
-              </ListItem>
-              <ListItem href="/docs/primitives/typography" title="Typography">
-                Styles for headings, paragraphs, lists...etc
-              </ListItem>
+            <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
             </ul>
           </NavigationMenuContent>
         </NavigationMenuItem>
